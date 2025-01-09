293c9ebec38417568aed5ae0a32ae3fda9b3e1a3
Khoa Le <binkhoa1812@gmail.com>
2025-01-09 11:29:02 +0700

    Update MNIST label and training mode;

--- mnist/build_and_train_model.py
+++ mnist/build_and_train_model.py
@@ -1,86 +1,76 @@
-# Build Model
 #y_true: tensor (samples, max_string_length) containing the truth labels.
 #y_pred: tensor (samples, time_steps, num_categories) containing the prediction, or output of the softmax.
 #input_length: tensor (samples, 1) containing the sequence length of slices coming out from RNN for each batch item in y_pred.
 #label_length: tensor (samples, 1) containing the sequence length of label for each batch item in y_true.
-def build_model(img_width = 128,img_height = 32, max_str_len = 10):
-    # Inputs to the model
-    input_img = layers.Input(
-        shape=(img_width, img_height, 1), name="image", dtype="float32"
-    ) 
-    # First conv block
-    x = layers.Conv2D(
-        32,
-        (3, 3),
-        activation="relu",
-        kernel_initializer="he_normal",
-        padding="same",
-        name="Conv1",
-    )(input_img)
+
+# Build and Train Model
+from tensorflow.keras import layers, Model
+from tensorflow import keras
+import tensorflow as tf
+
+def build_model(img_width=128, img_height=32, max_str_len=10, num_classes=len(alphabets) + 1):
+    # Input layer
+    input_img = layers.Input(shape=(img_width, img_height, 1), name="image", dtype="float32")
+    # First Convolutional Block
+    x = layers.Conv2D(32, (3, 3), activation="relu", kernel_initializer="he_normal", padding="same", name="Conv1")(input_img)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D((2, 2), name="pool1")(x)
-    # Second conv block
-    x = layers.Conv2D(
-        64,
-        (3, 3),
-        activation="relu",
-        kernel_initializer="he_normal",
-        padding="same",
-        name="Conv2",
-    )(x)
+    # Second Convolutional Block
+    x = layers.Conv2D(64, (3, 3), activation="relu", kernel_initializer="he_normal", padding="same", name="Conv2")(x)
+    x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D((2, 2), name="pool2")(x)
-    # We have used two max pool with pool size and strides 2.
-    # Hence, downsampled feature maps are 4x smaller. The number of
-    # filters in the last layer is 64. Reshape accordingly before
-    # passing the output to the RNN part of the model
+    # Reshape layer
     new_shape = ((img_width // 4), (img_height // 4) * 64)
     x = layers.Reshape(target_shape=new_shape, name="reshape")(x)
     x = layers.Dense(64, activation="relu", name="dense1")(x)
     x = layers.Dropout(0.2)(x)
-    # RNNs
+    # RNN layers
     x = layers.Bidirectional(layers.LSTM(128, return_sequences=True, dropout=0.25))(x)
     x = layers.Bidirectional(layers.LSTM(64, return_sequences=True, dropout=0.25))(x)
     # Output layer
-    y_pred = layers.Dense(10 + 1, activation="softmax", name="dense2")(x) # y pred
-    model = keras.models.Model(inputs=input_img, outputs=y_pred, name="functional_1")
+    y_pred = layers.Dense(num_classes, activation="softmax", name="dense2")(x)
+    model = Model(inputs=input_img, outputs=y_pred, name="ocr_model")
+    # CTC Loss Layer
+    labels = layers.Input(name="gtruth_labels", shape=[max_str_len], dtype="float32")
+    input_length = layers.Input(name="input_length", shape=[1], dtype="int64")
+    label_length = layers.Input(name="label_length", shape=[1], dtype="int64")
     def ctc_lambda_func(args):
         y_pred, labels, input_length, label_length = args
-        # the 2 is critical here since the first couple outputs of the RNN
-        # tend to be garbage
-        y_pred = y_pred[:, 2:, :]
+        y_pred = y_pred[:, 2:, :]  # Skip first 2 outputs
         return tf.keras.backend.ctc_batch_cost(labels, y_pred, input_length, label_length)
-    # Config label and input shape
-    labels = layers.Input(name='gtruth_labels', shape=[max_str_len], dtype='float32')
-    input_length = layers.Input(name='input_length', shape=[1], dtype='int64')
-    label_length = layers.Input(name='label_length', shape=[1], dtype='int64')
-    # Construct CTC loss
-    ctc_loss = keras.layers.Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
-    model_final = keras.models.Model(inputs=[input_img, labels, input_length, label_length], outputs=ctc_loss, name = "ocr_model_v1")
-    # Finalise
+
+    ctc_loss = layers.Lambda(ctc_lambda_func, output_shape=(1,), name="ctc")(
+        [y_pred, labels, input_length, label_length]
+    )
+    # Final model
+    model_final = Model(
+        inputs=[input_img, labels, input_length, label_length], outputs=ctc_loss, name="ocr_model_final"
+    )
     return model, model_final
-# Demo
+# Build Model
 model, model_final = build_model()
 model.summary()
 model_final.summary()
-
-# Train Model (modify setup as your preference)
-opt = keras.optimizers.Adam()
-early_stopping_patience = 5
-# Add early stopping
-early_stopping = keras.callbacks.EarlyStopping(
-    monitor="val_loss", patience=early_stopping_patience, restore_best_weights=True
-)
-# Compile model with optimizer (Adam)
+# Compile Model
 model_final.compile(
-    loss={'ctc': lambda y_true, y_pred: y_pred}, 
-    optimizer=keras.optimizers.Adam(learning_rate=0.0001)
+    loss={"ctc": lambda y_true, y_pred: y_pred},
+    optimizer=keras.optimizers.Adam(learning_rate=0.0001),
 )
-# Fit model epochs=200, batch_size=128
+# Early Stopping and Learning Rate Schedule
+early_stopping = keras.callbacks.EarlyStopping(
+    monitor="val_loss", patience=5, restore_best_weights=True
+)
+lr_schedule = keras.callbacks.ReduceLROnPlateau(
+    monitor="val_loss", factor=0.5, patience=3, min_lr=1e-6, verbose=1
+)
+# Train Model
 model_final.fit(
     x=[train_x, train_y, train_input_len, train_label_len],
-    y=train_output, 
+    y=train_output,
     validation_data=([valid_x, valid_y, valid_input_len, valid_label_len], valid_output),
-    epochs=200, 
+    epochs=200,
     batch_size=128,
-    callbacks=[early_stopping]  # Include early stopping to monitor val_loss
+    callbacks=[early_stopping, lr_schedule],
 )
-model.save('../models/mnistrcnn_m1.h5')
+# Save Model
+model_final.save("../models/mnistrcnn_m1.keras", save_format="keras")
--- mnist/prepare_label.py
+++ mnist/prepare_label.py
@@ -1,49 +1,52 @@
-# The labels have to be converted to numbers which represent each character in the training set. 
-# The 'alphabets' consist of A-Z and three special characters (- ' and space).
-alphabets = u"0123456789' "
-max_str_len = 10 # max length of input labels
-# My project have 7 digits per image, but as long as the max_str_len > the number of digit per image, 
-#it's just fine
-num_of_characters = len(alphabets) + 1 # +1 for ctc pseudo blank
-num_of_timestamps = 32 # max length of predicted labels
-# Converter of type
-def label_to_num(label):
+# Prepare Labels for CTC Loss
+import numpy as np
+
+def label_to_num(label, alphabets):
     label_num = []
     for ch in label:
+        if alphabets.find(ch) == -1:
+            print(f"Warning: Character '{ch}' not found in alphabets.")
         label_num.append(alphabets.find(ch))
     return np.array(label_num)
-def num_to_label(num):
+
+def num_to_label(num, alphabets):
     ret = ""
     for ch in num:
         if ch == -1:  # CTC Blank
             break
         else:
-            ret+=alphabets[ch]
+            ret += alphabets[ch]
     return ret
 
-name = '39816931'
-print(name, '\n',label_to_num(name))
+def prepare_labels(labels, size, max_str_len, num_of_timestamps, alphabets):
+    y = np.ones([size, max_str_len]) * -1
+    label_len = np.zeros([size, 1])
+    input_len = np.ones([size, 1]) * (num_of_timestamps - 2)
+    for i in range(size):
+        label_len[i] = len(labels[i])
+        y[i, 0:len(labels[i])] = label_to_num(labels[i], alphabets)
+    return y, label_len, input_len
 
-# Prepare labelling of train set for output
-train_y = np.ones([train_size, max_str_len]) * -1
-train_label_len = np.zeros([train_size, 1])
-train_input_len = np.ones([train_size, 1]) * (num_of_timestamps-2)
+# Configuration
+alphabets = u"0123456789' "
+max_str_len = 10  # Maximum length of input labels
+num_of_timestamps = 32  # Maximum length of predicted labels
+train_size = len(label_train)
+valid_size = len(label_valid)
+
+# Prepare Training and Validation Labels
+train_y, train_label_len, train_input_len = prepare_labels(label_train, train_size, max_str_len, num_of_timestamps, alphabets)
+valid_y, valid_label_len, valid_input_len = prepare_labels(label_valid, valid_size, max_str_len, num_of_timestamps, alphabets)
 train_output = np.zeros([train_size])
-# Label from img name
-for i in range(train_size):
-    train_label_len[i] = len(label_train[i])
-    train_y[i, 0:len(label_train[i])]= label_to_num(label_train[i])  
-# Prepare labelling of valid (test) set for output
-valid_y = np.ones([valid_size, max_str_len]) * -1
-valid_label_len = np.zeros([valid_size, 1])
-valid_input_len = np.ones([valid_size, 1]) * (num_of_timestamps-2)
 valid_output = np.zeros([valid_size])
-# Label from img name
-for i in range(valid_size):
-    valid_label_len[i] = len(label_valid[i])
-    valid_y[i, 0:len(label_valid[i])]= label_to_num(label_valid[i])   
-# Debugs
-print('\n True label_train  : ',label_train[10] , '\ntrain_y : ',train_y[10],
-      '\ntrain_label_len : ',train_label_len[10], '\ntrain_input_len : ', train_input_len[10])
-print('\n True label_valid : ',label_valid[10] , '\ntrain_y : ',valid_y[10],
-      '\ntrain_label_len : ',valid_label_len[10], '\ntrain_input_len : ', valid_input_len[10])
+
+# Debug Outputs
+print(f"Example Train Label: {label_train[10]}")
+print(f"Train Label Encoded: {train_y[10]}")
+print(f"Train Label Length: {train_label_len[10]}")
+print(f"Train Input Length: {train_input_len[10]}")
+
+print(f"Example Valid Label: {label_valid[10]}")
+print(f"Valid Label Encoded: {valid_y[10]}")
+print(f"Valid Label Length: {valid_label_len[10]}")
+print(f"Valid Input Length: {valid_input_len[10]}")
